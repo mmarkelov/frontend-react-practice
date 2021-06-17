3f6baa132498217f1ac327d15135ebd4ed5f5ee7
gepalova <gplva418@yandex.ru>
2021-06-18 01:39:49 +0300

    template form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import SignIn from "./mainForm";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+      <SignIn />
   </React.StrictMode>,
   document.getElementById('root')
 );
