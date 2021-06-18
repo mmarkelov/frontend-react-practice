54bba13b9ef061c588b28aa13e76f27f4d643e12
Jules <jules.adikhanyan@gmail.com>
2021-06-18 18:48:39 +0300

    adding tasks

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,8 @@
 const Main = () => {
   return (
-      <div>
-        Main
-      </div>
+      <>
+          Main
+      </>
   );
 }
 
--- src/pages/ToDoList/index.js
+++ src/pages/ToDoList/index.js
@@ -1,10 +1,30 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import Form from "../../components/Form";
+import List from "../../components/List";
+
+import {useState} from "react";
+import {makeStyles} from "@material-ui/styles";
+import {Box} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    tasks: {
+        display: "flex"
+    }
+
+});
 
 const ToDoList = () => {
-  return (
+    const classes = useStyles();
+    const [tasks, setTasks] = useState([]);
+
+    return (
       <>
         <Header />
+          <Box className={classes.tasks}>
+              <Form tasks={tasks} setTasks={setTasks}/>
+              <List tasks={tasks}/>
+          </Box>
         <Footer />
       </>
   );
