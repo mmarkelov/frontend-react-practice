ed76cfe143037948694b0349b8e0b3035817fd17
gepalova <gplva418@yandex.ru>
2021-06-22 03:51:03 +0300

    filter menu

--- src/pages/ToDoList/index.js
+++ src/pages/ToDoList/index.js
@@ -1,10 +1,12 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import TaskManager from "../../components/TaskManager";
 
 const ToDoList = () => {
   return (
       <>
         <Header />
+          <TaskManager />
         <Footer />
       </>
   );
